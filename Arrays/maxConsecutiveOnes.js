94fa1a9ab253405609b4d0ae5055bf92a91d40ea
dibya.borah <dibyajyotiborah469@gmail.com>
2025-02-16 16:38:30 +0530

    starting maximumConsecutiveOnes problem

---
+++ Arrays/maxConsecutiveOnes.js
@@ -0,0 +1,10 @@
+// 485. Max Consecutive Ones
+
+// Given a binary array nums, return the maximum number of consecutive 1's in the array.
+
+// Input: nums = [1,1,0,1,1,1]
+// Output: 3
+
+// Input: nums = [1,0,1,1,0,1]
+// Output: 2
+
