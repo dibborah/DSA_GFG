427ae8f77f593065295ce10c73d6207ee6ca7f44
dibya.borah <dibyajyotiborah469@gmail.com>
2025-02-16 15:43:07 +0530

    starting remove duplicates from sorted array

---
+++ Arrays/removeDuplicates.js
@@ -0,0 +1,37 @@
+// Remove duplicates from a sorted array
+
+// Leetcode
+// 26. Remove Duplicates from Sorted Array
+
+// Given an integer array nums sorted in non-decreasing order, remove the duplicates in-place such that each unique element appears only once. The relative order of the elements should be kept the same. Then return the number of unique elements in nums.
+
+// Consider the number of unique elements of nums to be k, to get accepted, you need to do the following things:
+
+// Change the array nums such that the first k elements of nums contain the unique elements in the order they were present in nums initially. The remaining elements of nums are not important as well as the size of nums.
+// Return k.
+// Custom Judge
+
+
+const nums1 = [10, 20, 20, 30, 30, 30, 30];
+const size = 7;
+
+// output:
+// nums1 = [10, 20, 30];
+// size = 3;
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
+
+class Solution {
+  fun(arr) {
+    
+  };
+};
+
+var removeDuplicates = function(nums) {
+  const solution = new Solution();
+  return solution.fun(nums);
+};
