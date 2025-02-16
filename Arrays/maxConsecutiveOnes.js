407316f918ab5e0d5566459618f424001021c232
dibya.borah <dibyajyotiborah469@gmail.com>
2025-02-16 16:54:05 +0530

    brute force approach of maxConsecutiveOnes done

--- Arrays/maxConsecutiveOnes.js
+++ Arrays/maxConsecutiveOnes.js
@@ -2,9 +2,39 @@
 
 // Given a binary array nums, return the maximum number of consecutive 1's in the array.
 
-// Input: nums = [1,1,0,1,1,1]
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+
 // Output: 3
 
 // Input: nums = [1,0,1,1,0,1]
 // Output: 2
 
+class Solution{
+  // brute force approach done
+  fun(nums) {
+    let ans = 0;
+    for(let i = 0; i < nums.length; i++) {
+      let temp = 0;
+      let k = i;
+      while(nums[k] === 1) {
+        temp++;
+        k++;
+      }
+      ans = Math.max(ans, temp);
+    };
+    return ans;
+  };
+};  
+
+var findMaxConsecutiveOnes = function(nums) {
+  const solution = new Solution();    
+  return solution.fun(nums);
+};
+
+// const nums = [1,1,0,1,1,1];
+const nums = [1,0,1,1,0,1];
+console.log(findMaxConsecutiveOnes(nums));
