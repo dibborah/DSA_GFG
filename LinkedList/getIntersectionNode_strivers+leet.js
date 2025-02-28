a0772fb3062b2db460e5fcc12b92ecb4de0cb4ac
dibya.borah <dibyajyotiborah469@gmail.com>
2025-02-28 14:26:27 +0530

    dummyNode approach done but it is not allowed as per the question

--- LinkedList/getIntersectionNode_strivers+leet.js
+++ LinkedList/getIntersectionNode_strivers+leet.js
@@ -23,16 +23,41 @@ class Node {
   };
 };
 
-const listA = new Node(4);
-listA.next = new Node(1);
-listA.next.next = new Node(8);
-listA.next.next.next = new Node(4);
-listA.next.next.next.next = new Node(5);
+// last test case(Equal nodes) test case
+const listA = new Node(2);
+listA.next = new Node(2);
+listA.next.next = new Node(4);
+listA.next.next.next = new Node(5);
+listA.next.next.next.next = new Node(4);
 
-const listB = new Node(5);
-listB.next = new Node(6);
-listB.next.next = new Node(1);
-listB.next.next.next = listA.next.next;
+const listB = new Node(2);
+listB.next = new Node(2);
+listB.next.next = new Node(4);
+listB.next.next.next = new Node(5);
+listB.next.next.next.next = listA.next.next.next.next;
+
+// 1st test case
+// const listA = new Node(4);
+// listA.next = new Node(1);
+// listA.next.next = new Node(8);
+// listA.next.next.next = new Node(4);
+// listA.next.next.next.next = new Node(5);
+
+// const listB = new Node(5);
+// listB.next = new Node(6);
+// listB.next.next = new Node(1);
+// listB.next.next.next = listA.next.next;
+
+// 2nd test case
+// const listA = new Node(1);
+// listA.next = new Node(9);
+// listA.next.next = new Node(1);
+// listA.next.next.next = new Node(2);
+// listA.next.next.next.next = new Node(4);
+
+// const listB = new Node(3);
+// listB.next = new Node(2);
+// listB.next.next = listA.next.next.next.next;
 
 class Solution {
   printLL(head) {
@@ -44,14 +69,97 @@ class Solution {
     };
     console.log(result.slice(0, -4)); // Remove trailing ' -> '
   };
-  fun(list1, list2) {
-  };
+  // Solution via adding a new property visited to the Node's structure
+//   fun(head1, head2) {
+//     let c1 = head1;
+//     let c2 = head2;
+//     while(c1 !== null && c2 !== null) {
+//       if(c1.visited) {
+//         return c1;
+//       };
+//       if(c2.visited) {
+//         return c2;
+//       }
+//       c1.visited = true;
+//       if(c2.visited) {
+//         return c2;
+//       }
+//       c2.visited = true;
+//       c1 = c1.next;
+//       c2 = c2.next;
+//     };
+//     while(c1 !== null) {
+//       if(c1.visited) {
+//         return c1;
+//       };
+//       c1.visited = true;
+//       c1 = c1.next;
+//     }
+//     while(c2 !== null) {
+//       if(c2.visited) {
+//         return c2;
+//       }
+//       c2.visited = true;
+//       c2 = c2.next;
+//     }
+//     return null;
+//   };
+
+// Note : Cannot use this dummyNode's approach as its is not allowed as per
+// the Leetcode question 
+// // solution via using a dummyNode
+//   fun(head1, head2) {
+//     const dummyNode = new Node(-1);
+//     let c1 = head1;
+//     let c2 = head2;
+//     while(c1 !== null && c2 !== null) {
+//       const nextC1 = c1.next;
+//       const nextC2 = c2.next;
+
+//       if(c1.next === dummyNode) {
+//         return c1;
+//       };
+//       if(c2.next === dummyNode) {
+//         return c2;
+//       };
+
+//       c1.next = dummyNode;
+//       if(c2.next === dummyNode) {
+//         return c2;
+//       };
+//       c2.next = dummyNode;
+//       c1 = nextC1;
+//       c2 = nextC2;
+//     };
+//     while(c1 !== null) {
+//       const nextC1 = c1.next;
+  
+//       if(c1.next === dummyNode) {
+//         return c1;
+//       };
+  
+//       c1.next = dummyNode;
+//       c1 = nextC1;
+//     };
+//     while(c2 !== null) {
+//       const nextC2 = c2.next;  
+//       if(c2.next === dummyNode) {
+//         return c2;
+//       };
+  
+//       c2.next = dummyNode;
+//       c2 = nextC2;
+//     };
+//     return null;
+//   };
+    fun(head1, head2) {
+      
+    };
 };
  
 const getIntersectionNode = function(headA, headB) {
-    const solution = new Solution();
-    solution.printLL(headA);
-    solution.printLL(headB);
+  const solution = new Solution();
+  return solution.fun(headA, headB);
 };
 
-getIntersectionNode(listA, listB);
+console.log(getIntersectionNode(listA, listB));
