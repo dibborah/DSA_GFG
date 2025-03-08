15dd3f37d33cc8cccab050f3a8bcad5c3edc1dc1
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-08 22:01:05 +0530

    Learned the naive solution for finding the LongestProperPrefixSuffix problem

--- Strings/lps.js
+++ Strings/lps.js
@@ -4,46 +4,156 @@
 // Write a program that takes a string as an arguement and return
 // a LPS arrays
 
-class Solution {
-  fun(str, n){
-    const result = [];
-    for (let i = 0; i < str.length; i++) {
-      let p = '';
-      let s = '';
+// Aditya verma
 
-      let p_ = [];
-      let s_ = []
+// recursive 
+// memoize
+// Tabulation
 
-      let j = 0, k = i;
+// class Solution {
+// // My Naive approach
+// // TC: n^3
+// // SC: n^3
+//   fun(str){
+//     const result = [];
+//     for (let i = 0; i < str.length; i++) {
+//       let p = '';
+//       let s = '';
 
-      while(j <= i && k >=0) {
-        p = str.slice(0, j);
-        p_.push(p);
-        s = str.slice(k, i + 1);
-        s_.push(s);
-        j++;
-        k--;
-      };
+//       let p_ = [];
+//       let s_ = []
 
-      let temp = 0;
+//       let j = 0, k = i;
 
-      for (let i = 0; i < s_.length; i++) {
-        let len = p_.includes(s_[i]) ? s_[i]?.length : 0;
-        temp = Math.max(temp, len);
-      };
+//       while(j <= i && k >=0) {
+//         p = str.slice(0, j);
+//         p_.push(p);
+//         s = str.slice(k, i + 1);
+//         s_.push(s);
+//         j++;
+//         k--;
+//       };
 
-      result.push(temp);
-    };
-    return result;
-  };
-};
+//       let temp = 0;
 
-let lps = function(str) {
-  const solution = new Solution();
-  return solution.fun(str, str.length);
-};
+//       for (let i = 0; i < s_.length; i++) {
+//         let len = p_.includes(s_[i]) ? s_[i]?.length : 0;
+//         temp = Math.max(temp, len);
+//       };
+
+//       result.push(temp);
+//     };
+//     return result;
+//   };
+
+// // Other naive solution 
+//   longProperPreSuff(str, n) {
+//     for (let len = n - 1; len > 0; len--) {
+//       let flag = true;
+//       for(let i = 0; i < len; i++) {
+//         if(str[i] !== str[n - len + i]) {
+//             flag = false;
+//         };
+//         if(flag) {
+//           return len;
+//         };
+//       };
+//     };
+//     return 0;
+//   };
+//   fun(str, lps) {
+//     for (let i = 0; i < str.length; i++) {
+//       lps[i] = this.longProperPreSuff(str, i + 1);
+//     };
+//   };
+// };
+
+// class Solution_ {
+    
+//     // Other naive solution 
+//       longProperPreSuff(str, n) {
+//         for (let len = n - 1; len > 0; len--) {
+//           let flag = true;
+//           for(let i = 0; i < len; i++) {
+//             if(str[i] !== str[n - len + i]) {
+//                 flag = false;
+//             };
+//             if(flag) {
+//               return len;
+//             };
+//           };
+//         };
+//         return 0;
+//       };
+//       fun(str, lps) {
+//         for (let i = 0; i < str.length; i++) {
+//           lps[i] = this.longProperPreSuff(str, i + 1);
+//         };
+//       };
+// };
+
+
+// let fn = function(str) {
+//   const lps = [];
+//   const solution = new Solution_();
+//   solution.fun(str, lps);
+//   // return solution.fun(str);
+//   return lps;
+// };
+
+// const str = "abab";
 
 // const str = "ababc";
 // const str = "abacabad";
-const str = "abbabb";
-console.log(lps(str))
+// const str = "abbabb";
+
+// console.log(fn(str));
+
+
+
+
+
+
+
+
+// GFG solution
+
+// Jab string equal nahi ha
+// to false ho k break hua hain
+
+class Solution2 {
+  longProperPreSuff(str, n) {
+    for (let len = n - 1; len > 0; len--) {
+      let flag = true;
+      for (let i = 0; i < len; i++) {
+        const k = n - len + i;
+        if (str[i] !== str[k]) {
+          flag = false;
+          break;
+        };
+      };
+      if (flag) {
+        return len;
+      };
+    };
+    return 0;
+  };
+
+  fun(str, lps) {
+      const n = str.length;
+      for (let i = 0; i < n; i++) {
+          lps[i] = this.longProperPreSuff(str, i + 1);
+      };
+  };
+};
+
+// const str = "abab";
+// const str = "abacabad";
+
+const str = "a b a b a c a b";
+
+const lps = [];
+const solution_ = new Solution2();
+solution_.fun(str, lps);
+console.log('lps', lps);
+
