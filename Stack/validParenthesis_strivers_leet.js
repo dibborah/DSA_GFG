db55c7780a1a3e7b7911aaef9d485c32da92be46
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-15 19:40:34 +0530

    Done valid parenthises problem

---
+++ Stack/validParenthesis_strivers_leet.js
@@ -0,0 +1,93 @@
+// Strivers
+// LeetCode problem number and name
+// 20. Valid Parentheses
+
+// Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+// An input string is valid if:
+
+// Open brackets must be closed by the same type of brackets.
+// Open brackets must be closed in the correct order.
+// Every close bracket has a corresponding open bracket of the same type
+
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+
+
+class MyStack {
+    constructor() {
+      this.items = [];
+    }
+  
+    push(element) {
+      this.items.push(element);
+    }
+  
+    pop() {
+      if (this.isEmpty()) {
+        return 'Stack is empty';
+      }
+      return this.items.pop();
+    }
+  
+    peek() {
+      if (this.isEmpty()) {
+        return 'Stack is empty';
+      }
+      return this.items[this.items.length - 1];
+    }
+  
+    isEmpty() {
+      return this.items.length === 0;
+    }
+  
+    size() {
+      return this.items.length;
+    }
+  
+    clear() {
+      this.items = [];
+    }
+  
+    print() {
+      console.log(this.items.join(' '));
+    }
+}
+
+class Solution{
+  fun(s) {
+    const stack = new MyStack();
+    for(let i = 0; i < s.length; i++) {
+        const ch = s.charAt(i);
+        if(ch === '(' || ch === '{' || ch === '[') {
+            stack.push(ch);
+        } else{
+            if(
+              (ch === ')' && stack.peek() === '(')
+              || (ch === '}' && stack.peek() === '{')
+              || (ch === ']' && stack.peek() === '[')
+            ) {
+                stack.pop();
+            }else {
+                return false;
+            };
+        };
+    };
+    return stack.isEmpty();
+  };
+};
+
+let isValid = function(s) {
+  const solution = new Solution();
+  return solution.fun(s);
+};
+
+const s = "()";
+// const s = "()[]{}"
+// const s  = "([])";
+// const s = "(]"
+// const s = '(';
+
+console.log(isValid(s));
