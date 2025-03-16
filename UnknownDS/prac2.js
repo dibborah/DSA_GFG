6b4a62eafdf0e3b4be021b643dd2420780d2a41c
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-16 13:43:20 +0530

    NGE of single array optimised 2N appoach T&S complexities done

--- UnknownDS/prac2.js
+++ UnknownDS/prac2.js
@@ -1,13 +1,13 @@
 // Next Greater element(NGE) of a single array
 
-const arr = [6, 0, 8, 1, 3];
+const arr1 = [6, 0, 8, 1, 3];
 
 // Finding the NGE of a single array
 // The TC: of this appoach is O(N^2)
 // SC: Is O(N). Here N is not used for the solution formation but instead to return the answer
 // So SC O(N) cannot be optimzed further since we have to return the answer
 // So manipulating the given array is not recommend as a good practice
-class Solution {
+class Solution1 {
   fun(nums) {
     const n = nums.length;
     const result = new Array(n).fill(-1);
@@ -23,5 +23,35 @@ class Solution {
   };
 };
 
-const solution = new Solution();
-console.log(solution.fun(arr));
+const solution = new Solution1();
+
+const arr2 = [4, 12, 5, 3, 1, 2, 5, 3, 1, 2, 4, 6];
+
+// Optimized solution using MONOTONIC STACK
+// TC at worst : O(2N): The inner loop will at worst will go to N in total not N every time the loop executes
+// So the TC is N + N not N^2
+// SC: is still O(2N)
+// Intuition: The light pool idea where a girl standing front will only see the light pools increasing ones, the decreasing ones in the middle will be hidden
+class Solution2 {
+  findNGE(arr) {
+    const n = arr.length;
+    const stack = [];
+    const nge = new Array(n).fill(0);
+    
+    for(let i = n - 1; i >= 0; i--) {
+        while (stack.length !== 0 && stack.at(-1) <= arr[i]) {
+          stack.pop();
+        };
+        if(stack.length === 0) {
+            nge[i] = -1;
+        } else {
+            nge[i] = stack.at(-1);
+        };
+        stack.push(arr[i]);
+    };
+    return nge;
+  };
+};
+
+const solution2 = new Solution2();
+console.log(solution2.findNGE(arr2));
