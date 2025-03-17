0bfe2041c265dbaecf18030a424d61e6c96a7876
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-17 17:25:46 +0530

    The intuition and ideas of the efficient histogram problem solution added

--- Stack/Monotonic_Stack/Histogram_largestRectangleArea_strivers_leet.js
+++ Stack/Monotonic_Stack/Histogram_largestRectangleArea_strivers_leet.js
@@ -113,6 +113,10 @@ let largestRectangleArea2 = function(heights) {
 // console.log(largestRectangleArea2(heights2));
 
 
+// This is the most efficient solution to this problem
+// TC: O(N) => outer loop & O(N) to pop out all the element from the stack 
+// Total TC: 2 * O(N) : O(2N)
+// SC: O(N) : For stack
 class Solution3 {
     fun(arr) {
       const stack = [];
@@ -142,6 +146,28 @@ class Solution3 {
       return maxArea;
     };
 };
+
+// Intuition: When I am in an element I need I have the previous smaller element(pse) of that element since I am traversed till that element 
+// But in no world I can have the next smaller element(nse) of that element since I have no idea of the elements on the higher indexes than the current index since I have not yet travered them
+// So the intuition is is some way I may have known the earlier indexes (some way I need the above elements information)
+// Being in the current element I can never know about the higher indexes elements
+// But: If I somehow move backwards than being in the backward elements I can have the information of some of the earlier elements in turn I may have the nse of the earlier elements when I am in a current index
+
+// Note: I need to traveler the entire array since I need all the elements information for me to know the area in each elements
+// Without traversing I can never have that information
+// That's the idea:
+// 1. traveler when the element in stack is greater or equal pop or kick it out of the stack . Kicking it out means touching it
+// Touching an element meaning now we will calculate its area : If it is kicking out it is not smaller but greater than the current element
+// So the current element is nse of the popped or kicked out element(consider the index).
+// we have the nse index of the popped element.
+// The pse will the top element of the stack or now or the previous index of the popped out element.
+// So calculate the area of the kicked/popped out element. 
+
+// lastly two idea: 
+// If the stack is empty in the middle will popping out the stack than the pse will be -1 since no previous stack elemet of the popped out element exist
+// If after travering the entire array some elements still remains in the stack
+// Then those elements nse doesnot exist , since thats the reason they where never kicked out of the stack and touched
+// So there nse will be length of the array(n)
 
 
 let largestRectangleArea3 = function(heights) {
