2f102a93a0dcfdb7eb3e3987fd139be082d1229d
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-18 17:03:07 +0530

    optmized: my Solution of increasing than decreasing using recursion

--- Recursion/print_1-n_n-1_bothIncreasingThanDecreasing.js
+++ Recursion/print_1-n_n-1_bothIncreasingThanDecreasing.js
@@ -1,10 +1,19 @@
+// class Solution {
+//     bothIncreasingNDecreasing(n, i) {
+//         if(i === n + 1) return n;
+//         console.log(i);
+//         const m = this.bothIncreasingNDecreasing(n, i + 1);
+//         console.log(m);
+//         return m - 1;
+//     };
+// };
+
 class Solution {
     bothIncreasingNDecreasing(n, i) {
-        if(i === n + 1) return n;
+        if(i === n + 1) return;
         console.log(i);
-        const m = this.bothIncreasingNDecreasing(n, i + 1);
-        console.log(m);
-        return m - 1;
+        this.bothIncreasingNDecreasing(n, i + 1);
+        console.log(i);
     };
 };
 
