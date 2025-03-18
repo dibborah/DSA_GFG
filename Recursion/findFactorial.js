8b47b37e782a468e4b485a8ce36051f6fc0546f4
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-18 17:09:48 +0530

    done factorial of a number using recursion

---
+++ Recursion/findFactorial.js
@@ -0,0 +1,10 @@
+class Solution {
+    findFactorial(n) {
+        if(n < 0) return 'Invalid input';
+        if(n === 0 || n === 1) return 1;
+        return n * this.findFactorial(n - 1);
+    };
+};
+
+const solution = new Solution();
+console.log(solution.findFactorial(-1));
