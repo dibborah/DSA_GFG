95833910e5545f85cdd7cc68795117cbd07f195b
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-21 20:21:31 +0530

    Done the brute force approach of getPermutation

--- Recursion/getPermutation_hard_strivers_leet.js
+++ Recursion/getPermutation_hard_strivers_leet.js
@@ -21,3 +21,43 @@
 // Input: n = 3, k = 3
 // Output: "213"
 
+class Solution {
+    swap(arr, i, j) {
+        const temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+    };
+
+    fun(arr, n, result, temp, index) {
+        if (index === n) {
+            result.push(arr.join(''));
+            return;
+        };
+
+        for(let i = index; i < n; i++) {
+            this.swap(arr, index, i);
+            this.fun(arr, n, result, temp, index + 1)
+            this.swap(arr, index, i);
+        };
+    };
+
+    permutation (n, k) {
+        const result = [];
+        const arr = [];
+        for(let i = 1; i <= n; i++) {
+            arr.push(i);
+        };
+
+        this.fun(arr, arr.length, result, [], 0);
+        result.sort((a, b) => a - b);
+        return result[k - 1];
+    };
+}
+
+const solution = new Solution();
+
+// const n = 3, k = 3;
+// const n = 4, k = 9;
+// const n = 3, k = 1;
+const n = 3, k = 5;
+console.log(solution.permutation(n, k));
