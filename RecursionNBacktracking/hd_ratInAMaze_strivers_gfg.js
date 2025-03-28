77374e25d8c6cc4c85b50380138427ec8b620738
dibya.borah <dibyajyotiborah469@gmail.com>
2025-03-28 12:18:55 +0530

    Done rat in a maze problem using approach I

--- RecursionNBacktracking/hd_ratInAMaze_strivers_gfg.js
+++ RecursionNBacktracking/hd_ratInAMaze_strivers_gfg.js
@@ -12,7 +12,7 @@
 
 // 0: A blocked cell through which the rat cannot travel.
 // 1: A free cell that the rat can pass through.
-// Note: In a path, no cell can be visited more than one time. If the source cell is 0, the rat cannot move to any other cell. In case of no path, return an empty list.+
+// Note: In a path, no cell can be vis more than one time. If the source cell is 0, the rat cannot move to any other cell. In case of no path, return an empty list.+
 
 // The task is to find all possible paths the rat can take to reach the destination, starting from (0, 0) and ending at (n-1, n-1), under the condition that the rat cannot revisit any cell along the same path. Furthermore, the rat can only move to adjacent cells that are within the bounds of the matrix and not blocked.
 
@@ -20,4 +20,90 @@
 
 // Input: mat[][] = [[1, 0, 0, 0], [1, 1, 0, 1], [1, 1, 0, 0], [0, 1, 1, 1]]
 // Output: ["DDRDRR", "DRDDRR"]
+
+
+// Need to understand the time complexity how it became 4^ (n * m)
+// TC: 4^(m * n)
+// Aux Recursion call stack SC: O(m * n)
+class Solution {
+  solve(i, j, mat, n, result, path, vis) {
+    if(i ===  n - 1 && j === n - 1) {
+      result.push(path);
+      return;
+    }
+
+    // Down
+   if ((i < n - 1) && (mat[i + 1][j] === 1) && !vis[i + 1][j]) {
+     vis[i + 1][j] = 1;           
+     this.solve(i + 1, j, mat, n, result, path + 'D', vis);
+     vis[i + 1][j] = 0;           
+    }
+
+    // Left
+    if ((j > 0) && (mat[i][j - 1] === 1) && !vis[i][j - 1]) {
+      vis[i][j - 1] = 1;           
+      this.solve(i, j - 1, mat, n, result, path + 'L', vis);
+      vis[i][j - 1] = 0;       
+    }
+
+    // Rigth
+    if ((j < n - 1) && (mat[i][j + 1] === 1) && !vis[i][j + 1]) {
+      vis[i][j + 1] = 1;           
+      this.solve(i, j + 1, mat, n, result, path + 'R', vis);
+      vis[i][j + 1] = 0;       
+    }
+
+    // Up
+    if ((i > 0) && (mat[i - 1][j] === 1) && !vis[i - 1][j]) {
+      vis[i - 1][j] = 1;           
+      this.solve(i - 1, j, mat, n, result, path + 'U', vis);
+      vis[i - 1][j] = 0;       
+    }
+
+  }
+
+  ratInAMaze(mat) {
+    const result = [];
+    const vis = [];
+
+    for(let i = 0; i < mat.length; i++) {
+      vis[i] = [];
+    }
+
+    vis[0][0] = 1;
+    (mat[0][0] === 1) && this.solve(0, 0, mat, mat.length, result, '', vis);
+      console.log('vis', vis[4][0]);
+    return result;
+  }
+};
+
+// Along with String char of Direction I also have to to pass the ways D|L|R|U
+
+// const mat = [
+//   [1, 0, 0, 0],
+//   [1, 1, 0, 1],
+//   [1, 1, 0, 0],
+//   [0, 1, 1, 1],
+// ];
+
+const mat = [
+  [1,1,1,0,1],
+  [1,0,1,1,1],
+  [0,0,1,1,1],
+  [1,0,0,1,1],
+  [1,0,0,0,1]
+];
+
+// Its correct output: RRDDRDRD RRDDRRDD RRDDRURDDD RRDRDDRD RRDRDRDD RRDRRDDD RRDRRDLDRD
+// My output: 
+// 'DURRDDRDRD',   'DURRDDRRDD',
+// 'DURRDDRURDDD', 'DURRDRDDRD',
+// 'DURRDRDRDD',   'DURRDRRDDD',
+// 'DURRDRRDLDRD', 'RRDDRDRD',
+// 'RRDDRRDD',     'RRDDRURDDD',
+// 'RRDRDDRD',     'RRDRDRDD',
+// 'RRDRRDDD',     'RRDRRDLDRD,
+
+const solution = new Solution();
+console.log(solution.ratInAMaze(mat));
 
